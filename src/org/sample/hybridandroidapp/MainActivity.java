9ab70885e0fc23cf96ed37cdffa89603dc2b030c
xx70235 <erkofs@gmail.com>
2016-08-16 15:58:00 +0800

    android版 Mob

--- src/org/sample/hybridandroidapp/MainActivity.java
+++ src/org/sample/hybridandroidapp/MainActivity.java
@@ -19,16 +19,64 @@
 
 package org.sample.hybridandroidapp;
 
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Toast;
+
 import org.apache.cordova.*;
+
 
 public class MainActivity extends CordovaActivity
 {
+    private WebView webview;
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        // Set by <content src="index.html" /> in config.xml
-        loadUrl(launchUrl);
+//        setContentView(R.layout.webview_layout);
+//        // Set by <content src="index.html" /> in config.xml
+//        loadUrl(launchUrl);
+
+        webview = new WebView(this);
+
+//设置WebView属性，能够执行Javascript脚本
+        final Activity activity = this;
+        webview.getSettings().setJavaScriptEnabled(true);
+        webview.setWebViewClient(new WebViewClient() {
+            public void onReceivedError(WebView view, int errorCode, String description, String failingUrl) {
+                Toast.makeText(activity, description, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+//加载需要显示的网页
+
+        webview.loadUrl("http://45.32.54.59/index.php?s=/mob/weibo/index.html");
+
+//设置Web视图
+
+        setContentView(webview);
+    }
+
+    @Override
+
+//设置回退
+
+//覆盖Activity类的onKeyDown(int keyCoder,KeyEvent event)方法
+
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+
+        if ((keyCode == KeyEvent.KEYCODE_BACK) && webview.canGoBack()) {
+
+            webview.goBack(); //goBack()表示返回WebView的上一页面
+
+            return true;
+
+        }
+
+        return false;
+
     }
 }
